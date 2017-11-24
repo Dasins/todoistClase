373051da2cef35fb8bf0a68675e19f0e4dc5d8fa
Jugador <dm.evilart@gmail.com>
2017-11-24 12:08:40 +0100

    Refactorizacion

--- Todoist.java
+++ Todoist.java
@@ -1,65 +1,72 @@
-import java.util.*;
+import java.util.ArrayList;
 /**
  * Clase que modela un objeto que permite
  * almacenar las tareas pendientes que tenemos por hacer.
  */
 public class Todoist {
-    // Nuestras tres tareas.
-    private String tarea0;
-    private String tarea1;
-    private String tarea2;
-    private ArrayList listaTareas;
+    // Almacena nuestras tareas.
+    private ArrayList<String> tareas;
     
     /**
      * Constructor de la clase Todoist:
-     * Crea un gestor con las tres tareas vacias.
+     * Crea un gestor de tareas vacio.
      */
-    public Todoist()
-    {
-        tarea0 = "";
-        tarea1 = "";
-        tarea2 = "";
-        List<String>listaTareas = new ArrayList();
+    public Todoist(){
+        tareas = new ArrayList<String>();
     }
     
     /**
      * Inserta la tarea especificada en la posición indicada.
-     * @param nombreTarea 
-     * @param posicion 
+     * @param nombreTarea La tarea que tenemos que realizar.
      */
-    public void nuevaTarea(String nombreTarea, int posicion)
-    {
-        if (posicion >= 0){
-           if (posicion == 0){
-               tarea0 = nombreTarea;
-           }
-           else if (posicion == 1){
-               tarea1 = nombreTarea;
-           }
-           else if (posicion >2){
-               tarea2 = nombreTarea;
-           }
+    public void nuevaTarea(String nombreTarea){
+        tareas.add(nombreTarea);
+    }
+    
+    /**
+     * Muestra por pantalla todas las tareas existentes.
+     */
+    public void mostrarTareas(){
+        System.out.println(tareas);
+    }
+    
+    /**
+     * @return Devuelve el numero de tareas pendientes.
+     */
+    public int getNumeroTareas(){
+        return tareas.size();  
+    }
+    
+    /**
+     * Imprime por pantalla el numero de tareas pendientes.
+     */
+    public void mostrarNumeroDeTareasPendientes(){
+        if (tareas.size() > 1){
+            System.out.println("Tienes" + tareas.size() + "pendientes.");
+        }
+        else {
+            System.out.println("Tiene" + tareas.size() + "pendiente.");
         }
     }
     
     /**
-     * Muestra por pantalla todas las tareas existentes
+     * Imprime por pantalla el numero de tareas pendientes.
      */
-    public void mostrarTareas()
-    {
-        String muestraTareas = "";
-        if (tarea0 != null){
-            muestraTareas = muestraTareas + tarea0 + "\n";
-        }
-        if (tarea1 != null){
-            muestraTareas = muestraTareas + tarea1 + "\n";
-        }
-        if (tarea2 != null){
-            muestraTareas = muestraTareas + tarea2 + "\n";
-        }
-        if (muestraTareas.length() > 0){
-            System.out.println(muestraTareas);
-        }
+    public void imprimirTareas(){
+       System.out.println(tareas);
     }
     
+    /**
+     * Elimina la tarea en la posición indicada
+     * @param posicion La posicion que ocupa la tarea que queremos borrar.
+     * @return Devuelve true si la posicion contiene un valor y falso si no.
+     */
+    public boolean eliminarTarea(int posicion){
+        boolean existeLaTarea = false;
+        if (posicion >= 0 && posicion < tareas.size()){
+            tareas.remove(posicion);
+            existeLaTarea = true;
+        }
+        return existeLaTarea; 
+    }
 }
